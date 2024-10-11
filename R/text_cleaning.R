bc0ec19bced2ec534a409c74313d2711b077d0de
Fonti Kar <f.kar@unsw.edu.au>
2024-10-12 10:47:47 +1100

    Cleaned Durston X Mid 1

--- R/text_cleaning.R
+++ R/text_cleaning.R
@@ -1,11 +1,24 @@
 extract_units <- function(vec){
   vec |> 
   str_trim() |> 
-  str_extract("[a-zA-Z]+")
+  str_extract("[a-zA-Z]+$")
+}
+
+remove_units <- function(vec){
+  vec |> 
+    str_trim() |> 
+    str_remove("[a-zA-Z]+$") |> 
+    str_trim() 
 }
 
 format_area_units <- function(vec){
   paste0(vec,"^2")
+}
+
+convert_numeric_value_to_char <- function(df){
+  df$value <- as.character(df$value)
+  
+  df
 }
 
 split_on_x_num_vals <- function(vec, index){
